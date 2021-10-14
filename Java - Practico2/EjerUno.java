a964a6fc9ed42d8e3bd01b5cdd084ecc24132709
Rodrigo <rod_hum_molina@hotmail.com>
2021-10-14 20:01:25 -0300

    master

---
+++ Java - Practico2/EjerUno.java
@@ -0,0 +1,13 @@
+
+public class EjerUno {
+        public static void main(String[] args) {
+           String [] ciudFavor = {"#1 - San Martín de los Andes", "#2 - Córdoba", "#3 - Posadas"};
+    
+            for (int i=0; i<3; i++) {
+                System.out.println (ciudFavor[i]);
+            }
+    
+        }
+       
+}
+
