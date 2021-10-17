ac351098008b66d18d9acc6ac7e0e0233e9425cf
Rodrigo <rod_hum_molina@hotmail.com>
2021-10-17 13:21:38 -0300

    master

---
+++ JavaPractico2/Ejercicio_5.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+    
+    public class Ejercicio_5 {
+    
+        public static void main(String[] args){
+            List<Integer> array1HorasTrabaj = new ArrayList<>();
+            List<Integer> array2ValorXhora = new ArrayList<>();
+            List<Integer> totales = new ArrayList<>();
+            int totalHs = 0;
+
+            array1HorasTrabaj.add(6);
+            array1HorasTrabaj.add(7);
+            array1HorasTrabaj.add(8);
+            array1HorasTrabaj.add(4);
+            array1HorasTrabaj.add(5);
+            array2ValorXhora.add(350);
+            array2ValorXhora.add(345);
+            array2ValorXhora.add(550);
+            array2ValorXhora.add(600);
+            array2ValorXhora.add(320);
+            
+                     
+            for (int i = 0; i < array1HorasTrabaj.size(); i++) {
+                int x  = array1HorasTrabaj.get(i) * array2ValorXhora.get(i);
+                totales.add(x);
+                System.out.print(x + " ");
+                totalHs = totalHs + x;
+            }
+           
+            System.out.println("\nTotal Final: $ " + totalHs);
+        }
+    }
+    
+
+
